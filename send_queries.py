fa94507ab70c4c081e603027aab6329b12220ff9
Amelia Bateman <amelia.bateman@hotmail.com>
2015-04-11 05:12:33 -0400

    Fixed JPGs!
    Updated send_queries.py so it now converts JPGs into base64 before
    running in URLs mode.
    
    Also put things in functions and cleaned everything up a bit

--- send_queries.py
+++ send_queries.py
@@ -8,27 +8,37 @@ results = {} #Mapping from input (url? jpg?) to [probability, label]
 labels = {}  #Mapping from label to [[inputs][probabilities]]
 
 #Sample data
-jpgs.append('http://img.foodnetwork.com/FOOD/2012/12/13/HE_Gift-Cookies-baked_s4x3_lead.jpg')
-jpgs.append('http://image.made-in-china.com/2f0j00mMNEsVDarHbj/Combination-Lock-5250-.jpg')
-jpgs.append('http://www.psdgraphics.com/file/combination-padlock-icon.jpg')
+jpgs.append('hamburger.jpg')
+jpgs.append('lock.jpg')
 
-while (len(jpgs) > 0):
-    print "Hi!"
-    input = jpgs.pop()
-    output = classifier.predict([input], input_type='urls')
-    probability = output[0].get(u'probability')
-    label = output[0].get(u'label')
-    results[input] = [probability, label]
-    if labels.has_key(label):
-        labels[label][0].append(input)
-        labels[label][1].append(probability)
-    else:
-        labels[label] = [[input],[probability]]
-    print 'Results[',input,']: ', results[input]
-    print 'Labels[',label,']: ', labels[label]
+#Get base64 encoding, which allows an image to work as a URL
+def toBase64 (jpg):
+    with open(jpg, "rb") as f:
+        data = f.read()
+        data_string = data.encode("base64")
+        return data_string
+
+def loop ():
+    while (len(jpgs) > 0):
+        print "Hi!"
+        input = jpgs.pop()
+        base64 = toBase64(input)
+        output = classifier.predict(['data:image/lock.jpg;base64,'+ base64], input_type='urls')
+        probability = output[0].get(u'probability')
+        label = output[0].get(u'label')
+        results[input] = [probability, label]
+        if labels.has_key(label):
+            labels[label][0].append(input)
+            labels[label][1].append(probability)
+        else:
+            labels[label] = [[input],[probability]]
+        print 'Results[',input,']: ', results[input]
+        print 'Labels[',label,']: ', labels[label]
+
+loop() #MAKE IT GO!
 
 #Using the classifier on a bunch of URLs, successful
-#print classifier.predict(['http://img.foodnetwork.com/FOOD/2012/12/13/HE_Gift-Cookies-baked_s4x3_lead.jpg', 'http://image.made-in-china.com/2f0j00mMNEsVDarHbj/Combination-Lock-5250-.jpg', 'https://kevinberardinelli.files.wordpress.com/2009/12/combo-lock.png'], input_type='urls')
+#print classifier.predict(['http://img.foodnetwork.com/FOOD/2012/12/13/HE_Gift-Cookies-baked_s4x3_lead.jpg'], input_type='urls')
 
-#Using the classifier on jpgs or pngs, which is sometimes BROKEN!!!
+#Using the classifier on jpgs or pngs, which is BROKEN!!! Must call toBase64
 #print classifier.predict(['combo-lock.png'], input_type='files')
